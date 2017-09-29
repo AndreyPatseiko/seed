48cc125a7febdea002c2119576bef10f76bb5720
Andrey Patseyko <andrey.patseyko@cactussoft.biz>
2017-09-29 18:33:39 +0300

    crypted file not finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import Mnemonic from 'bitcore-mnemonic';
 import bip39 from 'bip39';
 import bip32 from 'ripple-bip32';
 import CryptoJS from 'crypto-js';
+import * as nacl from 'tweetnacl';
+import base64 from 'base-64';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +20,7 @@ export class AppComponent implements OnInit {
   _balance;
   bip32 = bip32;
   bip39 = bip39;
+  nacl = nacl;
 
   constructor() {
     this.web3 = new Web3('http://192.168.11.214:5145');
@@ -32,9 +35,39 @@ export class AppComponent implements OnInit {
     const phrase = 'demise outdoor lesson swing birth century spike only chef owner lend embody'
     const key = 'EthWall'
     let hash = CryptoJS.SHA256(phrase);
-    console.log('hash = ' , hash.toString(CryptoJS.enc.Hex));
+    // console.log('hash = ' , hash.toString(CryptoJS.enc.Hex));
     let hash2 = CryptoJS.HmacSHA256(phrase, key);
-    console.log('hash2 = ' , hash2.toString(CryptoJS.enc.Hex));
+    // console.log('hash2 = ' , hash2.toString(CryptoJS.enc.Hex));
+
+    // =====    phrase to entropy and return frase    ========
+    // console.log('bip39 ', this.bip39)
+    // const mnemonicEntropy = this.bip39.mnemonicToEntropy(phrase);
+    // const praseFromEntropy = this.bip39.entropyToMnemonic(mnemonicEntropy);
+    // console.log('mnemonicEntropy = ', mnemonicEntropy);
+    // console.log('praseFromEntropy = ', praseFromEntropy);
+
+    // Шифрование фразы в файл
+      const _password = 'qweqweQ1'
+      const _mnemonicEntropy =  '3a53a601ee016a4af474d82753c5ffa4'
+      const _praseFromEntropy =  'demise outdoor lesson swing birth century spike only chef owner lend embody'
+
+      const _key = CryptoJS.SHA256(_password);
+      const _nonce = this.nacl.randomBytes(24)
+      // const _nonceStr = CryptoJS.enc.Base64.stringify(_nonce)
+
+      // const _cipher = this.nacl.secretbox(_mnemonicEntropy, _nonce, _key)
+
+      console.log('testPassword = ', _password, 'testKey 32 bytes =', _key, 'nonce = ', _nonce )
+      // console.log('nonceStr = ', _nonceStr, 'cipher =',  )
+
+      const finishNonce = "+Gyeghrvh6EtZXh7rKYLtHhJYfkRunrk";
+      const finishCipher = "JP9Kj7cCOCvY1EUVlvRPLpbvNuvD5sQkti+6uXX9aZDAkTDivp0KLGx8VLjWiLWdXlQCqpkqUE1kPBxpeFsGh9aiuPIZ0cso0C7DnQx1Yk2db/Eo4eMNx0c="
+
+      console.log(CryptoJS)
+      const cipher = base64.decode(finishCipher)
+      const nonce = base64.decode(finishNonce)
+      const _entropy = this.nacl.secretbox(cipher, nonce, _key)
+      console.log('cipher ', cipher, 'nonce ', nonce, '_entropy ', _entropy)
 
   }
 
